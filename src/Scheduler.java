6536f4df5297a4a7245cc6fec254758696d06232
Johnny322 <sipoperle@hotmail.com>
2020-03-03 10:10:47 +0100

    added functionality to Scheduler

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("ALL")
 public class Scheduler {
     private List<Group> football;
     private List<Group> basketball;
@@ -12,11 +13,11 @@ public class Scheduler {
 
     public void makeGroups(Sport _sport, List<Integer> groupIds) {
         int i = 0;
+        List<Team> teams1 = new ArrayList<>();
+        List<Team> teams2 = new ArrayList<>();
+        List<Team> teams3 = new ArrayList<>();
         switch (_sport) {
             case FOOTBALL:
-                List<Team> teams1 = new ArrayList<>();
-                List<Team> teams2 = new ArrayList<>();
-                List<Team> teams3 = new ArrayList<>();
                 if(groupIds.size()<13) {
                     while(i < groupIds.size()-1) {
                         if(i % 3 == 0) {
@@ -59,6 +60,91 @@ public class Scheduler {
                 }
                 break;
             case BASKETBALL:
+                if(groupIds.size()<13) {
+                    while(i < groupIds.size()-1) {
+                        if(i % 3 == 0) {
+                            teams1.add(new Team(groupIds.get(i)));
+                        }
+                        if(i % 3 == 1) {
+                            teams2.add(new Team(groupIds.get(i)));
+                        }
+                        if(i % 3 == 2) {
+                            teams3.add(new Team(groupIds.get(i)));
+                        }
+                        i++;
+                    }
+                    List<Group> _groups = new ArrayList<>();
+                    _groups.add(new Group(teams1, Sport.BASKETBALL));
+                    _groups.add(new Group(teams2, Sport.BASKETBALL));
+                    _groups.add(new Group(teams3, Sport.BASKETBALL));
+                } else {
+                    List<Team> teams4 = new ArrayList<>();
+                    while(i < groupIds.size()-1) {
+                        if(i % 4 == 0) {
+                            teams1.add(new Team(groupIds.get(i)));
+                        }
+                        if(i % 4 == 1) {
+                            teams2.add(new Team(groupIds.get(i)));
+                        }
+                        if(i % 4 == 2) {
+                            teams3.add(new Team(groupIds.get(i)));
+                        }
+                        if(i % 4 == 3) {
+                            teams4.add(new Team(groupIds.get(i)));
+                        }
+                        i++;
+                    }
+                    List<Group> _groups = new ArrayList<>();
+                    _groups.add(new Group(teams1, Sport.BASKETBALL));
+                    _groups.add(new Group(teams2, Sport.BASKETBALL));
+                    _groups.add(new Group(teams3, Sport.BASKETBALL));
+                    _groups.add(new Group(teams4, Sport.BASKETBALL));
+                }
+                break;
+            case VOLLEYBALL:
+                if(groupIds.size()<13) {
+                    while(i < groupIds.size()-1) {
+                        if(i % 3 == 0) {
+                            teams1.add(new Team(groupIds.get(i)));
+                        }
+                        if(i % 3 == 1) {
+                            teams2.add(new Team(groupIds.get(i)));
+                        }
+                        if(i % 3 == 2) {
+                            teams3.add(new Team(groupIds.get(i)));
+                        }
+                        i++;
+                    }
+                    List<Group> _groups = new ArrayList<>();
+                    _groups.add(new Group(teams1, Sport.VOLLEYBALL));
+                    _groups.add(new Group(teams2, Sport.VOLLEYBALL));
+                    _groups.add(new Group(teams3, Sport.VOLLEYBALL));
+                } else {
+                    List<Team> teams4 = new ArrayList<>();
+                    while(i < groupIds.size()-1) {
+                        if(i % 4 == 0) {
+                            teams1.add(new Team(groupIds.get(i)));
+                        }
+                        if(i % 4 == 1) {
+                            teams2.add(new Team(groupIds.get(i)));
+                        }
+                        if(i % 4 == 2) {
+                            teams3.add(new Team(groupIds.get(i)));
+                        }
+                        if(i % 4 == 3) {
+                            teams4.add(new Team(groupIds.get(i)));
+                        }
+                        i++;
+                    }
+                    List<Group> _groups = new ArrayList<>();
+                    _groups.add(new Group(teams1, Sport.VOLLEYBALL));
+                    _groups.add(new Group(teams2, Sport.VOLLEYBALL));
+                    _groups.add(new Group(teams3, Sport.VOLLEYBALL));
+                    _groups.add(new Group(teams4, Sport.VOLLEYBALL));
+                }
+                break;
         }
     }
+
+    public boolean evaluateMatch()
 }
